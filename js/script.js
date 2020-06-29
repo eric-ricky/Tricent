4e4c36b26e1048fddd2c87e0a89e292953a64573
Eric Ricky <ericricky200@gmail.com>
2020-06-28 22:07:25 -0700

    update

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(window).ready(function() {
 
     $(window).scroll(function() {
         $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
-        $('topBtn'.toggleClass('scrolled-top', $(this).scrollTop() > 250));
+        $('topBtn').toggleClass('scrolled-top', $(this).scrollTop() > 250);
     });
     $(window).scroll(function() {
 
